a1ccf685008a80682da11392637284f43e0c32fb
issei_ezawa <issemann@yahoo.co.jp>
2022-04-03 14:26:04 +0900

    Add: vue3 導入

---
+++ app/javascript/hello_vue/hello.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <p>{{ number }}</p>
+  <button @click="countUp">ここ</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "HelloVue!",
+      number: 0
+    }
+  },
+  methods: {
+    countUp: function() {
+      this.number += 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
